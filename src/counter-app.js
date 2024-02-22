458b33ca4fd15a06a0360cac9dfb20cdb8b8bff0
rjo01 <oommenryan4@gmail.com>
2024-02-21 19:38:37 -0500

    2-21 update

--- src/counter-app.js
+++ src/counter-app.js
@@ -10,7 +10,7 @@ export class CounterApp extends LitElement {
 
   static get properties() {
     return {
-        counter: { type: Number },
+        counter: { type: Number},
         min: {type: Number},
         max: {type: Number},
     };
@@ -19,8 +19,8 @@ export class CounterApp extends LitElement {
   constructor() {
     super();
     this.counter = 0;
-    this.min = 0;
-    this.max = 10;
+    this.min = 10;
+    this.max = 25;
   }
   
 
@@ -30,9 +30,7 @@ export class CounterApp extends LitElement {
     .counter{
         font-size: 20px;
     }
-    .counter-update{
-        color: blue;
-    }
+   
 
     .increase {
        background-color: green;
@@ -66,28 +64,68 @@ export class CounterApp extends LitElement {
         this.counter--;
     }
   }
-
-
-
-  updateCounter () {
-    const textElement = document.getElementById("counter");
-    if(this.counter == 18 || this.counter == 21){
-    textElement.style.color = "red"; 
+  
+  updateColor() {
+    if (this.counter == this.min || this.counter == this.max) {
+    this.style.color = 'red';
     }
+    else {
+    this.style.color = 'black';
+    }
+    if (this.counter == 18 || this.counter == 21) {
+    this.style.color = 'green';
+    }
+    else {
+    this.style.color = 'black';
+    }
+  } 
+
+
+
+makeItRain() {
+  // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+  // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+  // will only run AFTER the code is imported and available to us
+  import("@lrnwebcomponents/multiple-choice/lib/confetti-container.js").then(
+    (module) => {
+      // This is a minor timing 'hack'. We know the code library above will import prior to this running
+      // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+      // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+      // it's listening for changes so it can react
+      setTimeout(() => {
+        // forcibly set the poppped attribute on something with id confetti
+        // while I've said in general NOT to do this, the confetti container element will reset this
+        // after the animation runs so it's a simple way to generate the effect over and over again
+        this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+      }, 0);
+    }
+  );
+}
+updated(changedProperties) {
+  if (changedProperties.has('counter')) {
+    // do your testing of the value and make it rain by calling makeItRain
+    if(this.counter == 21) {
+    this.makeItRain();
+      }
+    // if(this.counter == 18) {
+    //   this.updateColor2();
+    // }
+
+   this.updateColor();
   }
-
-
-
+}
  
   render() {
     return html`
     <div class="counter-container">
         <h1>Counter-App</h1>
+        <confetti-container id="confetti">
             <div class="counter-app">
                 <div class="counter">${this.counter}</div>
-                <button class="increase" on @click="${this.add}">+</button>
-                <button class="decrease" @click="${this.subtract}">-</button>
+                <button class="increase" ?disabled="${this.max === this.counter}" on @click="${this.add}">+</button>
+                <button class="decrease" ?disabled="${this.min === this.counter}" on @click="${this.subtract}">-</button>
             </div> 
+    </confetti-container>
     </div>`;
     }
 
