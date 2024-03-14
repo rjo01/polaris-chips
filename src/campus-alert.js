cd59c4b8e8f8e9b31e24138623026c50f496abc7
rjo01 <oommenryan4@gmail.com>
2024-03-13 20:20:15 -0400

    Update campus-alert.js

--- src/campus-alert.js
+++ src/campus-alert.js
@@ -35,10 +35,10 @@ export class CampusAlert extends LitElement {
     }
   
   .alert-wrapper {
-    background-color: #efecef
+    background-color:#888888;
   }
   .date-wrapper{
-    background-color:#e5acf1;
+    background-color: #efecef;
     font-size:20px;
     font-weight:bold;
     padding:1px;
@@ -58,12 +58,19 @@ export class CampusAlert extends LitElement {
     top:0;
     right:0;
   }
-  .alert-message{
-    color:white;
+  .message{
+    color:grey;
+    font-size: 20px;
   }
     
     `;
   }
+
+  toggleAlert() { 
+    this.opened =!this.opened; 
+    localStorage.setItem("campus-alert-opened-state", this.opened);
+  } 
+
 openedAlert(color){
   return html`  
  <div class ="alert-wrapper" ?sticky="${this.sticky}"> 
@@ -86,6 +93,9 @@ closedAlert(){
   return html`
 <div class = "closed" ?sticky="${this.sticky}">
   <div class ="closed-toggle-button" @click="${this.toggleAlert}">
+  <svg xmlns="http://www.w3.org/2000/svg" style="height: 50px; width: 50px;" viewBox="0 0 24 24"><title>alert-circle-outline</title><path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z" /></svg>
+            New Alert!
+  <svg xmlns="http://www.w3.org/2000/svg" style="height: 50px; width: 50px;" viewBox="0 0 24 24"><title>chevron-down</title><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
   </div>
 </div> 
 `;}
